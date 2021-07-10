c212a8d0fc663d5adcaca9caef7d92d55e9911d3
Nihansh Reddy <Mrvijj1345@outlook.com>
2021-07-10 13:01:49 +0530

    adding weatherapp to git

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "eafd6956cd216a82a952464c6309f70a"
+    }
+}
+
+module.exports = constants;
